c0cfbcf482fbc61ed02d984527516a592da6f831
Ignacio Becerra <i1becerr@ucsd.edu>
2020-06-10 23:08:51 -0700

    Added username ID to database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 var cors = require('cors');
-var cookieParser = require('cookie-parser');
+var cookies = require('cookie-parser');
 const url = require('url');
 var request = require('request'); 
 var querystring = require('querystring');
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || "3000";
+app.use(cookies());
 
 var mysql = require('mysql');
 var db_config = {
@@ -44,56 +45,76 @@ handleDisconnect();
 var code = '';
 
 // redirect to HTML homepage
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
+
+  let query = "SELECT refresh_token FROM tokens WHERE refresh_token='" + req.cookies['refresh_token'] +"';";
+  connection.query(query, function(err, result, fields) {
+    if(result.length > 0) {
+      return res.redirect('/hub');
+    }
+
     res.sendFile(path.join(__dirname + '/public/index.html'));
+  });
+  
 });
+
+app.get('/hub', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/hub.html'));
+})
 
 // Obtain authorization code
 app.get("/callback", (req, res) => {
   const queryObject = url.parse(req.url,true).query;
   authCode = queryObject.code;
 
+  
+
+  generate_tokens(res, function(response) {
+    var access_token = response.access_token;
+        refresh_token = response.refresh_token;
+    
+    var authOptions = {
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      },
+      json: true
+    };
+
+    request.get(authOptions, function(error, response, body) {
+      var sql = "INSERT INTO tokens (access_token, refresh_token, id) VALUES ('"+access_token+
+                "','"+refresh_token+"', '" + body.id + "');";
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log('1 entry added')
+        res.send('memes');
+      });
+    });
+  })
+
+});
+
+function generate_tokens(res, callback) {
   var authOptions = {
-  	url: 'https://accounts.spotify.com/api/token',
-  	form: {
-  	  client_id: process.env.client_id,
-  	  client_secret: process.env.client_secret,
-  	  code: authCode,
-  	  redirect_uri: 'https://synchronicity115.herokuapp.com/callback',
-  	  grant_type: 'authorization_code'
-  	},
-  	json: true
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      client_id: process.env.client_id,
+      client_secret: process.env.client_secret,
+      code: authCode,
+      redirect_uri: 'http://localhost:3000/callback',
+      grant_type: 'authorization_code'
+    },
+    json: true
   };
   
   request.post(authOptions, function(error, response, body) {
 
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var sql = "INSERT INTO tokens (access_token, refresh_token) VALUES ('"+access_token+"','"+refresh_token+"')";
-        connection.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("1 record inserted");
-        });
-
-        var time = 0;
-        var options = {
-          url: 'https://api.spotify.com/v1/me/player/seek?position_ms='+time,
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true,
-        };
-
-        // use the access token to access the Spotify Web API
-        request.put(options, function(error, response, body) {
-        });
-      }
-    });
-
-res.send('eme')
-
-});
+    if (!error && response.statusCode === 200) {
+      res.cookie('refresh_token', body.refresh_token);
+      callback(body)
+    }
+  });
+}
 
 app.get('/refresh_token', function(req, res) {
 
@@ -120,13 +141,23 @@ app.get('/refresh_token', function(req, res) {
 });
 
 
-app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
+function reset_song() {
+  var time = 0;
+  var options = {
+    url: 'https://api.spotify.com/v1/me/player/seek?position_ms='+time,
+    headers: { 'Authorization': 'Bearer ' + access_token },
+    json: true,
+  };
+
+  // use the access token to access the Spotify Web API
+  request.put(options, function(error, response, body) {
+  });
+}
+
 
 app.get('/login', function(req, res) {
-	var scopes = 'user-read-currently-playing user-modify-playback-state';
-	var redirect_uri = 'https://synchronicity115.herokuapp.com/callback'
+	var scopes = 'user-read-currently-playing user-modify-playback-state user-read-email';
+	var redirect_uri = 'http://localhost:3000/callback'
 
 	res.redirect('https://accounts.spotify.com/authorize' +
 	  '?response_type=code' +
@@ -136,3 +167,7 @@ app.get('/login', function(req, res) {
 
 });
 
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
